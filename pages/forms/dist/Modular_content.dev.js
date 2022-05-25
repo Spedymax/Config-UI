2aceeb5e28d9bf0d115b05ec79e21befaebe863d
Spedymax <sokolov.maksim10@gmail.com>
2022-05-25 16:56:22 +0200

    ok

--- pages/forms/dist/Modular_content.dev.js
+++ pages/forms/dist/Modular_content.dev.js
@@ -1,72 +1,68 @@
 "use strict";
 
+// eslint-disable-next-line compat/compat
 fetch('data.json').then(function (response) {
   return response.json();
 }).then(function (data) {
-  ModularContent(data);
-  VaultConfiguration(data);
-})["catch"](function (err) {
-  console.log('error: ' + err);
+  modularContent(data);
+  vaultConfiguration(data);
+})["catch"](function (error) {
+  console("error: ".concat(error));
 });
 
-function ModularContent(data) {
-  var getElem1 = document.getElementById("CONTENT_MODULE_VAULT_QUERY_name");
+function modularContent(data) {
+  var getElem1 = document.getElementById('CONTENT_MODULE_VAULT_QUERY_name');
   getElem1.innerHTML = data[0].CONTENT_MODULE_VAULT_QUERY[0].name;
-  var getElem3 = document.getElementById("IS_CONTENT_MODULES_HIDDEN_name");
+  var getElem3 = document.getElementById('IS_CONTENT_MODULES_HIDDEN_name');
   getElem3.innerHTML = data[0].IS_CONTENT_MODULES_HIDDEN[0].name;
 
-  if (data[0].CONTENT_MODULE_VAULT_QUERY[0].type === "string") {
+  if (data[0].CONTENT_MODULE_VAULT_QUERY[0].type === 'string') {
     var input = document.createElement('input');
-    input.type = "txt";
-    input.className = "form-control";
-    input.placeholder = "Value";
+    input.type = 'txt';
+    input.className = 'form-control';
+    input.placeholder = 'Value';
     var okay = document.querySelector('#CONTENT_MODULE_VAULT_QUERY_type_id');
     okay.appendChild(input);
-    console.log("It works!");
   }
 
-  if (data[0].IS_CONTENT_MODULES_HIDDEN[0].type === "boolean") {
+  if (data[0].IS_CONTENT_MODULES_HIDDEN[0].type === 'boolean') {
     var select = document.createElement('select');
-    select.className = "form-control";
+    select.className = 'form-control';
     var option1 = document.createElement('option');
     var option2 = document.createElement('option');
-    option1.textContent = "True";
-    option2.textContent = "False";
+    option1.textContent = 'True';
+    option2.textContent = 'False';
     select.appendChild(option1);
     select.appendChild(option2);
     var okay2 = document.querySelector('#IS_CONTENT_MODULES_HIDDEN_type_id');
     okay2.appendChild(select);
-    console.log("It works!");
   }
 }
 
-function VaultConfiguration(data) {
-  var getElem1 = document.getElementById("VAULT_LOGIN_URL_name");
+function vaultConfiguration(data) {
+  var getElem1 = document.getElementById('VAULT_LOGIN_URL_name');
   getElem1.innerHTML = data[1].VAULT_LOGIN_URL[0].name;
-  var getElem3 = document.getElementById("VAULT_VERSION_name");
+  var getElem3 = document.getElementById('VAULT_VERSION_name');
   getElem3.innerHTML = data[1].VAULT_VERSION[0].name;
 
-  if (data[1].VAULT_LOGIN_URL[0].type === "string") {
+  if (data[1].VAULT_LOGIN_URL[0].type === 'string') {
     var input = document.createElement('input');
-    input.type = "txt";
-    input.className = "form-control";
-    input.placeholder = "Value";
+    input.type = 'txt';
+    input.className = 'form-control';
+    input.placeholder = 'Value';
     var okay = document.querySelector('#VAULT_LOGIN_URL_type_id');
     okay.appendChild(input);
-    console.log("It works!");
   }
 
-  if (data[1].VAULT_VERSION[0].type === "string") {
+  if (data[1].VAULT_VERSION[0].type === 'string') {
     var _input = document.createElement('input');
 
-    _input.type = "txt";
-    _input.className = "form-control";
-    _input.placeholder = "Value";
+    _input.type = 'txt';
+    _input.className = 'form-control';
+    _input.placeholder = 'Value';
 
     var _okay = document.querySelector('#VAULT_VERSION_type_id');
 
     _okay.appendChild(_input);
-
-    console.log("It works!");
   }
 }
--- pages/forms/modular_content.js
+++ pages/forms/modular_content.js
@@ -1,78 +1,67 @@
+// eslint-disable-next-line compat/compat
 fetch('data.json')
-.then(function (response) {
-    return response.json();
+.then(response => {
+  return response.json()
 })
-.then(function (data) {
-   ModularContent(data);
-   VaultConfiguration(data);
+.then(data => {
+  modularContent(data)
+  vaultConfiguration(data)
 })
-.catch(function (err) {
-    console.log('error: ' + err);
-});
+.catch(error => {
+  console(`error: ${error}`)
+})
 
+function modularContent(data) {
+  const getElem1 = document.getElementById('CONTENT_MODULE_VAULT_QUERY_name')
+  getElem1.innerHTML = data[0].CONTENT_MODULE_VAULT_QUERY[0].name
+  const getElem3 = document.getElementById('IS_CONTENT_MODULES_HIDDEN_name')
+  getElem3.innerHTML = data[0].IS_CONTENT_MODULES_HIDDEN[0].name
 
- function ModularContent(data) {
-    var getElem1 = document.getElementById("CONTENT_MODULE_VAULT_QUERY_name");
-    getElem1.innerHTML =  data[0].CONTENT_MODULE_VAULT_QUERY[0].name;
-    var getElem3 = document.getElementById("IS_CONTENT_MODULES_HIDDEN_name");
-    getElem3.innerHTML =  data[0].IS_CONTENT_MODULES_HIDDEN[0].name;
-    
+  if (data[0].CONTENT_MODULE_VAULT_QUERY[0].type === 'string') {
+    const input = document.createElement('input')
+    input.type = 'txt'
+    input.className = 'form-control'
+    input.placeholder = 'Value'
+    const okay = document.querySelector('#CONTENT_MODULE_VAULT_QUERY_type_id')
+    okay.appendChild(input)
+  }
 
-    if(data[0].CONTENT_MODULE_VAULT_QUERY[0].type === "string"){
-        let input = document.createElement('input');
-        input.type = "txt"
-        input.className = "form-control"
-        input.placeholder="Value"
-        const okay = document.querySelector('#CONTENT_MODULE_VAULT_QUERY_type_id');
-        okay.appendChild(input)
-        console.log("It works!")
-        
-    }
-
-    if(data[0].IS_CONTENT_MODULES_HIDDEN[0].type === "boolean"){
-        let select = document.createElement('select');
-        select.className = "form-control"
-        let option1 = document.createElement('option');
-        let option2 = document.createElement('option');
-        option1.textContent = "True"
-        option2.textContent = "False"
-        select.appendChild(option1);
-        select.appendChild(option2);
-        const okay2 = document.querySelector('#IS_CONTENT_MODULES_HIDDEN_type_id');
-        okay2.appendChild(select)
-        console.log("It works!")
-        
-    }
+  if (data[0].IS_CONTENT_MODULES_HIDDEN[0].type === 'boolean') {
+    const select = document.createElement('select')
+    select.className = 'form-control'
+    const option1 = document.createElement('option')
+    const option2 = document.createElement('option')
+    option1.textContent = 'True'
+    option2.textContent = 'False'
+    select.appendChild(option1)
+    select.appendChild(option2)
+    const okay2 = document.querySelector('#IS_CONTENT_MODULES_HIDDEN_type_id')
+    okay2.appendChild(select)
+  }
 }
 
-function VaultConfiguration(data) {
-    var getElem1 = document.getElementById("VAULT_LOGIN_URL_name");
-    getElem1.innerHTML =  data[1].VAULT_LOGIN_URL[0].name;
+function vaultConfiguration(data) {
+  const getElem1 = document.getElementById('VAULT_LOGIN_URL_name')
+  getElem1.innerHTML = data[1].VAULT_LOGIN_URL[0].name
 
-    var getElem3 = document.getElementById("VAULT_VERSION_name");
-    getElem3.innerHTML =  data[1].VAULT_VERSION[0].name;
+  const getElem3 = document.getElementById('VAULT_VERSION_name')
+  getElem3.innerHTML = data[1].VAULT_VERSION[0].name
 
-    
+  if (data[1].VAULT_LOGIN_URL[0].type === 'string') {
+    const input = document.createElement('input')
+    input.type = 'txt'
+    input.className = 'form-control'
+    input.placeholder = 'Value'
+    const okay = document.querySelector('#VAULT_LOGIN_URL_type_id')
+    okay.appendChild(input)
+  }
 
-    if(data[1].VAULT_LOGIN_URL[0].type === "string"){
-        let input = document.createElement('input');
-        input.type = "txt"
-        input.className = "form-control"
-        input.placeholder="Value"
-        const okay = document.querySelector('#VAULT_LOGIN_URL_type_id');
-        okay.appendChild(input)
-        console.log("It works!")
-        
-    }
-
-    if(data[1].VAULT_VERSION[0].type === "string"){
-        let input = document.createElement('input');
-        input.type = "txt"
-        input.className = "form-control"
-        input.placeholder="Value"
-        const okay = document.querySelector('#VAULT_VERSION_type_id');
-        okay.appendChild(input)
-        console.log("It works!")
-        
-    }
+  if (data[1].VAULT_VERSION[0].type === 'string') {
+    const input = document.createElement('input')
+    input.type = 'txt'
+    input.className = 'form-control'
+    input.placeholder = 'Value'
+    const okay = document.querySelector('#VAULT_VERSION_type_id')
+    okay.appendChild(input)
+  }
 }
